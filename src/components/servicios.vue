1b727be452bf5e67cd19a2ed99d31a7c4668505f
Javier <javierjaldo@gmail.com>
2020-12-01 18:39:52 -0300

    Finalazacion servicios

--- src/components/servicios.vue
+++ src/components/servicios.vue
@@ -120,7 +120,7 @@
       </v-card>
     </v-col>
 
-     <v-col sm="1" md="4" lg="3" class="mx-5">
+     <v-col sm="1" md="4" lg="3" class="mx-5 mb-10">
       <v-card
         elevation="3"
         outlined
@@ -130,14 +130,86 @@
       >
         <v-card-text>
           <h2 class="text--primary">Análisis de activos </h2>
-
+          <p></p>
           <div class="text--primary">
             Análisis de activos y pasivos de la compañía.
-          </div>
+          </div><p></p>
         </v-card-text>
         <v-card-actions>
           <v-btn text color="teal accent-4" @click="reveal = true">
             <i style="font-size: 40px" class="far fa-building"></i>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+
+    <v-col sm="1" md="4" lg="3" class="mx-5 ">
+      <v-card
+        elevation="3"
+        outlined
+        shaped
+        min-height="250px"
+        color="blue-grey lighten-5"
+      >
+       <v-card-text>
+          <h2 class="text--primary">Estructuración</h2>
+     
+          <div class="text--primary">
+            Estructuración de planes estratégicos que colaboren en la correlación 
+            entre el flujo de ingresos y las cuentas a pagar de la compañía.
+          </div><p></p>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text color="teal accent-4" @click="reveal = true">
+            <i style="font-size: 40px" class="fas fa-coins"></i>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+
+
+     <v-col sm="1" md="4" lg="3" class="mx-5">
+      <v-card
+        elevation="3"
+        outlined
+        shaped
+        min-height="250px"
+        color="blue-grey lighten-5"
+      >
+        <v-card-text>
+          <h2 class="text--primary">Estructura de capital</h2>
+          <div class="text--primary">
+           Optimizar la estructura de capital con el objetivo de financiar actividades de acuerdo al 
+           flujo de ingresos a generar con la inversión y al plazo de recupero de la misma.
+          </div><p></p>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text color="teal accent-4" @click="reveal = true">
+            <i style="font-size: 40px" class="fab fa-stack-exchange"></i>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+
+     <v-col sm="1" md="4" lg="3" class="mx-5">
+      <v-card
+        elevation="3"
+        outlined
+        shaped
+        min-height="250px"
+        color="blue-grey lighten-5"
+      >
+        <v-card-text>
+          <h2 class="text--primary"> Redes de venta</h2>
+          
+          <div class="text--primary">
+            Armado de redes de venta y comercialización en todo el país con asistencia
+             financiera directa al consumidor final.
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text color="teal accent-4" @click="reveal = true">
+            <i style="font-size: 40px" class="fas fa-share-alt-square"></i>
           </v-btn>
         </v-card-actions>
       </v-card>
