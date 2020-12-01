279ba1559ef96c6a5ece707a0b0200d10d620266
Javier <javierjaldo@gmail.com>
2020-11-30 22:52:56 -0300

    Mas servicios

--- src/components/servicios.vue
+++ src/components/servicios.vue
@@ -1,140 +1,161 @@
 <template>
-
-  <v-row justify="center"
-  
-  >
- 
-    <v-col
-      cols="4"
-      sm="3"
-      md="3"
-      lg="3"
-     
-      
-      
-   
-    >
+  <v-row justify="center">
+    <v-col cols="4" sm="3" md="3" lg="3" class="mx-5 mb-10">
       <v-card
-  elevation="5"
-  outlined
-  shaped
-  pr-10
-  min-height="280px"
-  color="blue-grey lighten-5"
-
-  
->
-
-<v-card-text>
-    
-      <h2 class="text--primary">
-        Organización
-      </h2>
-      
-      <div class="text--primary">
-        Organización y estructuración del flujo <br>de caja de la compañía.
-        
-      </div>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn
-        text
-        color="teal accent-4"
-        @click="reveal = true"
-        align-center
+        elevation="3"
+        outlined
+        shaped
+        pr-10
+        min-height="250px"
+        color="blue-grey lighten-5"
       >
-       <i style="font-size:40px;" class="fas fa-paper-plane"></i>
-      </v-btn>
-    </v-card-actions>
-</v-card>
-</v-col>
+        <v-card-text>
+          <h2 class="text--primary">Organización</h2>
 
-<v-col
-      cols="4"
-      sm="3"
-      md="3"
-      lg="3"
-    
-    >
-<v-card
-  elevation="5"
-  outlined
-  shaped
-   min-height="280px"
-   color="blue-grey lighten-5"
->
+          <div class="text--primary">
+            Organización y estructuración del flujo <br />de caja de la
+            compañía.
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text color="teal accent-4" @click="reveal = true" align-center>
+            <i style="font-size: 40px" class="fas fa-paper-plane"></i>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
 
-<v-card-text>
-     
-      <h2 class="text--primary">
-        Análisis de costos
-      </h2>
-      <p></p>
-      <div class="text--primary">
-        Análisis de costos de oportunidad y
-       rentabilidad de trabajos que realiza cada compañía.
-      </div>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn
-        text
-        color="teal accent-4"
-        @click="reveal = true"
+    <v-col cols="4" sm="3" md="3" lg="3" class="mx-5">
+      <v-card
+        elevation="3"
+        outlined
+        shaped
+        min-height="250px"
+        color="blue-grey lighten-5"
       >
-        <i style="font-size:40px;" class="fas fa-chart-pie"></i>
-      </v-btn>
-    </v-card-actions>
-</v-card>
-</v-col>
+        <v-card-text>
+          <h2 class="text--primary">Análisis de costos</h2>
+          <p></p>
+          <div class="text--primary">
+            Análisis de costos de oportunidad y rentabilidad de trabajos que
+            realiza cada compañía.
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text color="teal accent-4" @click="reveal = true">
+            <i style="font-size: 40px" class="fas fa-chart-pie"></i>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
 
-<v-col
-      cols="4"
-      sm="3"
-      md="3"
-      lg="3"
-    
-    >
-<v-card
-  elevation="5"
-  outlined
-  shaped
-  min-height="280px"
-  color="blue-grey lighten-5"
->
-
-<v-card-text>
-     
-      <h2 class="text--primary">
-        Elaboración de modelos 
-      </h2>
-    
-      <div class="text--primary">
-      Elaboración de modelos económicos/ financieros que permitan que la compañía pueda decidir si afrontar o no un trabajo.
-      </div>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn
-        text
-        color="teal accent-4"
-        @click="reveal = true"
+    <v-col cols="4" sm="3" md="3" lg="3" class="mx-5">
+      <v-card
+        elevation="3"
+        outlined
+        shaped
+        min-height="250px"
+        color="blue-grey lighten-5"
       >
-       <i style="font-size:40px;" class="fas fa-wallet"></i>
-      </v-btn>
-    </v-card-actions>
-</v-card>
+        <v-card-text>
+          <h2 class="text--primary">Elaboración de modelos</h2>
 
-</v-col>
+          <div class="text--primary">
+            Elaboración de modelos económicos/ financieros que permitan que la
+            compañía pueda decidir si afrontar o no un trabajo.
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text color="teal accent-4" @click="reveal = true">
+            <i style="font-size: 40px" class="fas fa-wallet"></i>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+
+ <v-col cols="4" sm="3" md="3" lg="3" class="mx-5">
+      <v-card
+        elevation="3"
+        outlined
+        shaped
+        min-height="250px"
+        color="blue-grey lighten-5"
+      >
+        <v-card-text>
+          <h2 class="text--primary">Elaboración de modelos</h2>
+
+          <div class="text--primary">
+            Elaboración de modelos económicos/ financieros que permitan que la
+            compañía pueda decidir si afrontar o no un trabajo.
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text color="teal accent-4" @click="reveal = true">
+            <i style="font-size: 40px" class="fas fa-wallet"></i>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+
+
+     <v-col cols="4" sm="3" md="3" lg="3" class="mx-5">
+      <v-card
+        elevation="3"
+        outlined
+        shaped
+        min-height="250px"
+        color="blue-grey lighten-5"
+      >
+        <v-card-text>
+          <h2 class="text--primary">Elaboración de modelos</h2>
+
+          <div class="text--primary">
+            Elaboración de modelos económicos/ financieros que permitan que la
+            compañía pueda decidir si afrontar o no un trabajo.
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text color="teal accent-4" @click="reveal = true">
+            <i style="font-size: 40px" class="fas fa-wallet"></i>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+
+     <v-col cols="4" sm="3" md="3" lg="3" class="mx-5">
+      <v-card
+        elevation="3"
+        outlined
+        shaped
+        min-height="250px"
+        color="blue-grey lighten-5"
+      >
+        <v-card-text>
+          <h2 class="text--primary">Elaboración de modelos</h2>
+
+          <div class="text--primary">
+            Elaboración de modelos económicos/ financieros que permitan que la
+            compañía pueda decidir si afrontar o no un trabajo.
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text color="teal accent-4" @click="reveal = true">
+            <i style="font-size: 40px" class="fas fa-wallet"></i>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
 
   </v-row>
+
+  
 </template>
 
 <style>
-h2{
-
-  padding-bottom:15px;
+h2 {
+  padding-bottom: 15px;
   font-weight: 400;
   font-size: 28px;
 }
-
 </style>
 
